c767250a0a347f26c22db9b3c167e0f9f4d9313d
Gourab <gourabgaming111@gmail.com>
2024-03-02 21:22:02 +0530

    testing taking input

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, redirect, url_for, render_template, request
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 app = Flask(__name__)
 
@@ -6,5 +8,30 @@ app = Flask(__name__)
 def form():
     return render_template('index.html')
 
+
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    if request.method == 'POST':
+        indus = float(request.form['indus'])
+        nox = float(request.form['nox'])
+        rm = float(request.form['rm'])
+        age = float(request.form['age'])
+        dis = float(request.form['dis'])
+        tax = float(request.form['tax'])
+        ptratio = float(request.form['ptratio'])
+        lstat = float(request.form['lstat'])
+    # array = np.array([indus, nox, rm, age, dis, tax, ptratio, lstat])
+    data = {'indus': indus, 'nox': nox, 'rm': rm, 'age': age, 'dis': dis, 'tax': tax, 'ptratio': ptratio, 'lstat': lstat}
+    return redirect(url_for('print', input= data))
+
+
+@app.route('/print/<input>')
+def print(input):
+    return render_template('print.html', data=input)
+
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
