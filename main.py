b003ae1ea4299225bfa5d20d4a7e1523ce8c47cf
Gourab <gourabgaming111@gmail.com>
2024-03-03 20:47:41 +0530

    load model, data processing, output py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, redirect, url_for, render_template, request
 from sklearn.preprocessing import MinMaxScaler
 import numpy as np
+import pickle
 
 app = Flask(__name__)
+
+model = pickle.load('models/model.pkl', 'rb')
 
 @app.route('/')
 def form():
@@ -11,25 +14,12 @@ def form():
 
 @app.route('/submit', methods=['POST', 'GET'])
 def submit():
-    if request.method == 'POST':
-        indus = float(request.form['indus'])
-        nox = float(request.form['nox'])
-        rm = float(request.form['rm'])
-        age = float(request.form['age'])
-        dis = float(request.form['dis'])
-        tax = float(request.form['tax'])
-        ptratio = float(request.form['ptratio'])
-        lstat = float(request.form['lstat'])
-    # array = np.array([indus, nox, rm, age, dis, tax, ptratio, lstat])
-    data = {'indus': indus, 'nox': nox, 'rm': rm, 'age': age, 'dis': dis, 'tax': tax, 'ptratio': ptratio, 'lstat': lstat}
-    return redirect(url_for('print', input= data))
-
-
-@app.route('/print/<input>')
-def print(input):
-    return render_template('print.html', data=input)
-
-
+    data = [float(x) for x in request.form.values()]
+    x = [np.array(data)]
+    x_scaled = MinMaxScaler.fit_transform(x)
+    prediction = model.predict(x_scaled)
+    
+    return render_template('print.html', price=prediction)
 
 
 
