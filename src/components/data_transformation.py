e8213053cd2bdad306d8c64cd852019618fb3120
Simpl1fy <gourabdas07012004@gmail.com>
2024-03-20 13:21:33 +0530

    finished data transformation and added save_object() to utils.py

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,9 +1,12 @@
 import os
 import sys
 from dataclasses import dataclass
+import pandas as pd
+import numpy as np
 
 from src.exception import CustomException
 from src.logger import logging
+from src.utils import save_object
 
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.pipeline import Pipeline
@@ -22,13 +25,13 @@ class DataTransformation:
         self.data_transformation_config = DataTransformationConfig()
     
 
-    def get_data_transformation_object():
+    def get_data_transformation_object(self):
         '''
         This function is responsible for returning the data transformation object
         '''
 
         try:
-            input_column = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+            input_column = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
             input_pipeline = Pipeline(steps=[
                 ("min_max_scaler", MinMaxScaler())
             ])
@@ -45,5 +48,48 @@ class DataTransformation:
             return (
                 preprocessor
             )
+        except Exception as e:
+            raise CustomException(e, sys)
+    
+
+    def initiate_data_transformation(self, train_path, test_path):
+        
+        try:
+            train_df = pd.read_csv(train_path)
+            test_df = pd.read_csv(test_path)
+
+            logging.info("read train and test csv as dataframe")
+            logging.info("obtaining preprocessing object")
+
+            target_column_name = 'MEDV'
+            input_column_name = ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM', 'AGE', 'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
+
+            preprocessing_obj = self.get_data_transformation_object()
+
+            input_feature_train_df = train_df.drop(columns=[target_column_name], axis=1)
+            target_feature_train_df = train_df[target_column_name]
+
+            input_feature_test_df = test_df.drop(columns=[target_column_name], axis=1)
+            target_feature_test_df = test_df[target_column_name]
+
+            logging.info("applying the preprocessor object to training and testing dataset")
+
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
+
+            train_arr = np.c_(input_feature_train_arr, np.array(target_feature_train_df))
+            test_arr = np.c_(input_feature_test_arr, np.array(target_feature_test_df))
+
+            save_object(
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj = preprocessing_obj
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path
+            )
+
         except Exception as e:
             raise CustomException(e, sys)
