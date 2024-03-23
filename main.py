7243d35807865bb73df861db02eb0953349b4542
Simpl1fy <gourabdas07012004@gmail.com>
2024-03-23 21:34:55 +0530

    added function to main.py and updated index and print html

--- main.py
+++ main.py
@@ -17,19 +17,25 @@ def predict_datapoint():
     if request.method == 'GET':
         return render_template('print.html')
     else:
-        data = CustomData(
-                indus = float(request.form.get('indus')),
-                nox = float(request.form.get('nox')),
-                rm = float(request.form.get('rm')),
-                age = float(request.form.get('age')),
-                tax = float(request.form.get('tax'),
-                ptratio = float(request.form.get('ptratio'))
-            )
+        data = CustomData(indus=float(request.form.get('indus')),
+                    nox=float(request.form.get('nox')),
+                    rm=float(request.form.get('rm')),
+                    age=float(request.form.get('age')),
+                    tax=float(request.form.get('tax')),
+                    ptratio=float(request.form.get('ptratio'))
+                )
+        
+        
+        pred_df = data.get_data_as_dataframe()
+        print(pred_df)
+        print("Before prediction")
 
-                
+        predict_pipeline = PredictPipeline()
+        print("Mid prediction")
+        results = predict_pipeline.predict(pred_df)
+        print("prediction completed")
+        return render_template('print.html', result=results[0])
      
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
